8867b941f987fe1905d2247832ead3e4d29c73a8
Ayatullah Rachman <115455741+AyatullahRachman@users.noreply.github.com>
2024-12-18 10:12:25 +0700

    Initial commit

---
+++ Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Camera mainCamera;
+
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Input untuk pergerakan kiri/kanan
+        float moveX = Input.GetAxisRaw("Horizontal");
+        moveDirection = new Vector3(moveX, 0, 0).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        // Gerakkan karakter berdasarkan input
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y);
+    }
+}
