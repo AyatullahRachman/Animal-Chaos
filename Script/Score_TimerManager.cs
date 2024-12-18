0779508b712d543f997cf525df92852d478e4945
Ayatullah Rachman <115455741+AyatullahRachman@users.noreply.github.com>
2024-12-18 11:13:06 +0700

    Update Material

--- Script/Score_TimerManager.cs
+++ Script/Score_TimerManager.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemy;
+    [SerializeField] private GameObject score;
+    [SerializeField] private GameObject timer;
 
-    public float spawnRate = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
